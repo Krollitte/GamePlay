105145a9b4d652a93cea27d9f189b7b80c91abb0
SantosRafa <rsantoslfap@gmail.com>
2021-06-21 11:26:45 -0300

    initial commit

---
+++ src/screens/Signin/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, Text, Image, StatusBar, TouchableOpacity } from "react-native";
+import { styles } from "./styles";
+
+import IllustrationImg from "../../assets/illustration.png";
+import { ButtonIcon } from "../../components/ButtonIcon";
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor="transparent"
+        translucent
+      />
+      <Image
+        resizeMode="stretch"
+        source={IllustrationImg}
+        style={styles.image}
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {`\n`} suas GamePlays {`\n`} facilmente
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {`\n`} favoritos com seus amigos{" "}
+        </Text>
+
+        <ButtonIcon title={"Entrar com Discord"} activeOpacity={0.7} />
+      </View>
+    </View>
+  );
+}
